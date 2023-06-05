d912d04c2302a9169205e270669a4f8af6f855c3
JuniorWow <junior@wowdesarrollos.com>
2023-06-05 13:29:03 -0500

    actualizando modelos

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,14 +3,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
+  const {token} = req.headers;
   if (!token) {
     return res.status(401).json({ msg: "Token no proporcionado" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    req._idUser = decoded.user._id;
+    req.headers._id = decoded.user._id;
     next();
   } catch (error) {
     return res.status(401).json({ msg: "Token inválido" });
--- routes/citas.js
+++ routes/citas.js
@@ -2,6 +2,7 @@ const express = require("express");
 const verifyToken = require("../middleware/middleware");
 const citas = express.Router();
 const CitasSchema = require("../model/modeloCitas");
+const UsuarioSchema = require("../model/modeloUsuario");
 
 //CREAR CITA
 citas.post("/", verifyToken, async (req, res) => {
@@ -21,15 +22,79 @@ citas.post("/", verifyToken, async (req, res) => {
     res.status(500).json({ msg: "error" });
   }
 });
-
-//MOSTRAR CITAS
+//OBTENER CITA
 citas.get("/", verifyToken, async (req, res) => {
   const { _id } = req.headers;
-  CitasSchema.find({ _idUser: `${_id}` })
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ msg: error }));
-});
 
+  try {
+    const usuario = await UsuarioSchema.findOne({ _id: _id });
+    const rolUsuario = usuario.role;
+    if (rolUsuario === "Admi") {
+      CitasSchema.find({})
+        .populate({
+          path: "_idUser",
+          populate: {
+            path: "_idtypeOfDocument",
+            select: "typeOfDocument",
+          },
+        })
+        .populate({
+          path: "specialty",
+          populate: {
+            path: "specialty",
+            select: "",
+          },
+        })
+        .populate({
+          path: "doctor",
+          populate: {
+            path: "name",
+            select: "",
+          },
+        })
+        .exec()
+        .then((data) => {
+          res.json(data);
+        })
+        .catch((error) => {
+          res.json({ msg: error });
+        });
+    } else {
+      CitasSchema.find({ _idUser: `${_id}` })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ msg: error }));
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Error al obtener el usuario" });
+  }
+});
+//aCTUALIZAR ESTADO CITA
+citas.put("/state/:id", verifyToken, async (req, res) => {
+  const { id } = req.params;
+  const { _id } = req.headers;
+  const { state } = req.body;
+  try {
+    const usuario = await UsuarioSchema.findOne({ _id: _id });
+    if (!usuario) {
+      return res.status(404).json({ msg: "Usuario no encontrado" });
+    }
+    const rolUsuario = usuario.role;
+    if (rolUsuario === "Admi") {
+      await CitasSchema.updateOne({ _id: id }, { $set: { state } });
+      res.json({
+        msg: `El estado de la cita se actualizó a ${
+          state ? "ACTIVA" : "INACTIVA"
+        }`,
+      });
+    } else {
+      res
+        .status(403)
+        .json({ msg: "No tienes permisos para realizar esta acción" });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Error al obtener el usuario", error });
+  }
+});
 //ELIMINAR CITA
 citas.delete("/:id", verifyToken, async (req, res) => {
   const { id } = req.headers;
